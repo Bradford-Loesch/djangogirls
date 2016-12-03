8658560e156d43ae1c04fb37d2c20dc58cd33a80
Bradford Loesch <bradford.loesch@gmail.com>
2016-12-03 11:10:03 -0600

    initial commit

---
+++ py3django/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\aethelwulf\documents\coding\djangogirls\py3django\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
